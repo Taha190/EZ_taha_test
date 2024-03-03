0e38e4da16024932dc5582eea24a7c336710cf74
Taha Warsi <taha.warsi19@gmail.com>
2024-03-04 01:24:26 +0500

    Push changes

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import SigninForm from "../components/SigninForm";
+import CommentSection from "../components/CommentSection";
+import AttachmentSection from '../components/Attachments';
+const attachments = [
+    { id: 1, title: 'Frame6.jpg', date: 'Sep 7 at 2:42 PM' },
+    // ... other attachments
+  ];
+
 
 function Signin() {
     return (
         <div>
-            <Navbar/>
-            <SigninForm/>
+             <Navbar/>
+            <SigninForm/> 
+            {/* <CommentSection/> */}
+            {/* <AttachmentSection /> */}
         </div>
     );
 }
